b418265d2b33bb97d54d4a3cf9261d26ad0efe0d
IT24103690 <IT24103690@my.sliit.lk>
2025-09-02 10:03:03 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24103690/IT24103690.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\IT24103690\\Desktop\\IT24103690")
+
+#1 
+  #I - Binomial Distribution
+  #II - 
+      1-pbinom(46, 50, 0.85, lower.tail = TRUE)
+      
+#2
+  #I - Number of calls per hour
+  #II - Poisson distribution
+  #III - 
+      dpois(15,12)
